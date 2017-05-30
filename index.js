60abed314f99bd1022b17e1f3b2c77a8d6b70108
Luke Wilson <luke@broughtbackbooks.com>
2017-05-30 09:54:31 -0500

    Correct mistake in for of loop

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var Instapaper = require('instapaper')
-var client     = Instapaper(CONSUMER_KEY, CONSUMER_SECRET)
+var client     = Instapaper('78f374062f5146e8a0fd0e11a3598431', '130a174a1ab4406d966d76e1d4e17cd0')
 
-client.setUserCredentials('lukewil', 'Ben71212')
+client.setUserCredentials('luke@broughtbackbooks.com', 'Ben71212')
 
 // Load a list of bookmarks using promises...
 client.bookmarks.list().then(function(bookmarks) {
-  console.log(bookmarks)
+  for (bookmark of bookmarks) {
+  console.log(bookmark.title)
+  }
 }).catch(function(err) {
   console.warn('oh noes', err)
 })
